1e923f9205fe7ab825575b35d02178533a818b70
Gasyh3 <kevrakt@gmail.com>
2022-12-07 17:47:16 +0100

    Upload Dashboard

---
+++ apiMabytDashboard/src/main/java/com/fk/apiMabytDashboard/ApiMabytDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.fk.apiMabytDashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMabytDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMabytDashboardApplication.class, args);
+	}
+
+}
---
+++ apiMabytDashboard/src/main/java/com/fk/apiMabytDashboard/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.fk.apiMabytDashboard;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ApiMabytDashboardApplication.class);
+	}
+
+}
